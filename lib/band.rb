8c5f3d542c1695355be8720d0d456c615181dad8
audrie <audrie.talbot@gmail.com>
2015-05-20 19:04:53 +0000

    Pass the title_case spec for band class.

--- lib/band.rb
+++ lib/band.rb
@@ -2,4 +2,15 @@ class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   
   validates(:name, {:presence => true})
+  before_save(:title_case)
+
+private
+
+  define_method(:title_case) do
+    split_name = self.name.split()
+    split_name.each() do |word|
+      word.capitalize!()
+    end
+    self.name = split_name.join(" ")
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -5,4 +5,8 @@ describe(Band) do
     band = Band.new({:name => ""})
     expect(band.save()).to(eq(false))
   end
+  it('capitalizes the first letter of every word in the band name') do
+    band = Band.create({:name => 'the beetles'})
+    expect(band.name()).to(eq('The Beetles'))
+  end
 end
